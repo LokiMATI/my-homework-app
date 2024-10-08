b63009d43fe60343091b8fdcbbb2ee843a5613da
LokiMATI <r.matigorov@yandex.ru>
2024-10-08 18:14:53 +0300

    Homework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+// Компоненты
+const Greeting = (props) => {
+  const {name, setName} = useState('');
+  return (
+    <div>
+      <h1>Hello, {props.name}!</h1>
+    </div>
+  );
+}
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(count + 1)}>  
+        Увеличить
+      </button>
+      <button onClick={() => setCount(count - 1)}>  
+        Уменьшить
+      </button>
+      <button onClick={() => setCount(0)}>  
+        Обнулить
+      </button>
+    </div>
+  );
+}
+
+const UserInfo = (props) => {
+  const [name, setName] = useState(props.name);
+  const [age, setAge] = useState(props.age);
+  const [flag, setFlag] = useState(false);
+
+  const changeFlag = (e) => {
+    e.preventDefault();
+    setFlag(!flag);
+  };
+  return (
+    <div>
+      <button onClick={changeFlag}>Информация о авторе</button>
+      {
+          flag ? (
+            <div>
+              <h1>Имя: {name}</h1>
+              <h1>Возраст: {age}</h1>
+              <button onClick={() => {setAge(Math.floor(Math.random() * (70 - 20 + 1)) + 20)}}> Изменить возраст</button>
+            </div>
+          ) : null
+      }
+    </div>
+      
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <dev>
+      <Greeting name="Nikia" />
+      <Counter />
+      <UserInfo name="Nikia" age="17" />
+    </dev>
   );
 }
 
